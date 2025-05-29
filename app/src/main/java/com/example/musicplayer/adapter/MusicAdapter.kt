550021c02b1d0cd499b3d2b6618783797c5db661
Khuất Tuấn Anh <22028248@vnu.edu.vn>
2025-05-29 20:52:31 +0700

    more_feature 1

--- app/src/main/java/com/example/musicplayer/adapter/MusicAdapter.kt
+++ app/src/main/java/com/example/musicplayer/adapter/MusicAdapter.kt
@@ -2,34 +2,24 @@ package com.example.musicplayer.adapter
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.text.SpannableStringBuilder
-import android.text.format.DateUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.text.bold
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.musicplayer.model.Music
-import com.example.musicplayer.onprg.PlayNext
 import com.example.musicplayer.onprg.PlaylistDetails
 import com.example.musicplayer.R
-import com.google.android.material.snackbar.Snackbar
 import com.example.musicplayer.adapter.MusicAdapter.MyHolder
 import com.example.musicplayer.activity.MainActivity
 import com.example.musicplayer.activity.PlayerActivity
 import com.example.musicplayer.onprg.PlaylistActivity
-import com.example.musicplayer.databinding.DetailsViewBinding
 import com.example.musicplayer.databinding.MusicViewBinding
 import com.example.musicplayer.fragment.MoreFeaturesBottomSheet
 import com.example.musicplayer.utils.formatDuration
-import com.example.musicplayer.utils.setDialogBtnBackground
 
 
 class MusicAdapter(private val context: Context, private var musicList: ArrayList<Music>, private val playlistDetails: Boolean = false,
@@ -43,6 +33,7 @@ class MusicAdapter(private val context: Context, private var musicList: ArrayLis
         val image = binding.imageMV
         val duration = binding.songDuration
         val root = binding.root
+        val moreInfoButton = binding.moreInfoButtonMV
     }
 
     private var onItemClick: ((Int) -> Unit)? = null
@@ -56,6 +47,15 @@ class MusicAdapter(private val context: Context, private var musicList: ArrayLis
         return MyHolder(MusicViewBinding.inflate(LayoutInflater.from(context), parent, false))
     }
 
+    private fun showMoreFeaturesBottomSheet(musicId: String) {
+        if (context is AppCompatActivity) {
+            val moreFeaturesSheet = MoreFeaturesBottomSheet.newInstance(musicId)
+            moreFeaturesSheet.show(context.supportFragmentManager, MoreFeaturesBottomSheet.TAG)
+        } else {
+            Toast.makeText(context, "Cannot show more features here", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
         holder.title.text = musicList[position].title
         holder.album.text = musicList[position].album
@@ -65,42 +65,43 @@ class MusicAdapter(private val context: Context, private var musicList: ArrayLis
             .apply(RequestOptions().placeholder(R.drawable.music_player_icon_slash_screen).centerCrop())
             .into(holder.image)
 
-        //for play next feature
-        if(!selectionActivity)
-            holder.root.setOnLongClickListener {
-                if (context is AppCompatActivity) {
-                    // Pass the music ID to the bottom sheet
-                    val moreFeaturesSheet = MoreFeaturesBottomSheet.newInstance(musicList[position].id)
-                    moreFeaturesSheet.show(context.supportFragmentManager, MoreFeaturesBottomSheet.TAG)
-                } else {
-                    Toast.makeText(context, "Cannot show more features here", Toast.LENGTH_SHORT).show()
-                }
-
-                return@setOnLongClickListener true
+        // Set OnClickListener for the moreInfoButton
+        if (!selectionActivity) {
+            holder.moreInfoButton.setOnClickListener {
+                showMoreFeaturesBottomSheet(musicList[position].id)
             }
+        }
 
-        when{
-            playlistDetails ->{
+        when {
+            playlistDetails -> {
                 holder.root.setOnClickListener {
                     sendIntent(ref = "PlaylistDetailsAdapter", pos = position)
                 }
             }
-            selectionActivity ->{
+            selectionActivity -> {
                 holder.root.setOnClickListener {
-                    if(addSong(musicList[position]))
+                    if (addSong(musicList[position]))
                         holder.root.setBackgroundColor(ContextCompat.getColor(context, R.color.cool_pink))
                     else
                         holder.root.setBackgroundColor(ContextCompat.getColor(context, R.color.white))
-
                 }
             }
-            else ->{
+            else -> {
                 holder.root.setOnClickListener {
-                    when{
+                    when {
                         MainActivity.search -> sendIntent(ref = "MusicAdapterSearch", pos = position)
                         musicList[position].id == PlayerActivity.nowPlayingId ->
                             sendIntent(ref = "NowPlaying", pos = PlayerActivity.songPosition)
-                        else->sendIntent(ref="MusicAdapter", pos = position) } }
+                        else -> sendIntent(ref = "MusicAdapter", pos = position)
+                    }
+                }
+                // Add long-click listener for non-selection and non-playlistDetails cases
+                if (!selectionActivity) { // Ensure not in selection mode
+                    holder.root.setOnLongClickListener {
+                        showMoreFeaturesBottomSheet(musicList[position].id)
+                        true // Consume the long click
+                    }
+                }
             }
         }
     }
@@ -109,20 +110,22 @@ class MusicAdapter(private val context: Context, private var musicList: ArrayLis
         return musicList.size
     }
 
-    fun updateMusicList(searchList : ArrayList<Music>){
+    fun updateMusicList(searchList: ArrayList<Music>) {
         musicList = ArrayList()
         musicList.addAll(searchList)
         notifyDataSetChanged()
     }
-    private fun sendIntent(ref: String, pos: Int){
+
+    private fun sendIntent(ref: String, pos: Int) {
         val intent = Intent(context, PlayerActivity::class.java)
         intent.putExtra("index", pos)
         intent.putExtra("class", ref)
         ContextCompat.startActivity(context, intent, null)
     }
-    private fun addSong(song: Music): Boolean{
+
+    private fun addSong(song: Music): Boolean {
         PlaylistActivity.musicPlaylist.ref[PlaylistDetails.currentPlaylistPos].playlist.forEachIndexed { index, music ->
-            if(song.id == music.id){
+            if (song.id == music.id) {
                 PlaylistActivity.musicPlaylist.ref[PlaylistDetails.currentPlaylistPos].playlist.removeAt(index)
                 return false
             }
@@ -130,7 +133,8 @@ class MusicAdapter(private val context: Context, private var musicList: ArrayLis
         PlaylistActivity.musicPlaylist.ref[PlaylistDetails.currentPlaylistPos].playlist.add(song)
         return true
     }
-    fun refreshPlaylist(){
+
+    fun refreshPlaylist() {
         musicList = ArrayList()
         musicList = PlaylistActivity.musicPlaylist.ref[PlaylistDetails.currentPlaylistPos].playlist
         notifyDataSetChanged()
